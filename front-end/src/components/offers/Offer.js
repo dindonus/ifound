f8fb8fa41239ee9154a93299b634f1556051c2c2
Nicolas Cordin <nicolas.cordin@habitatpresto.com>
2017-10-24 16:40:44 +0200

    responsive offers with grid

--- front-end/src/components/offers/Offer.js
+++ front-end/src/components/offers/Offer.js
@@ -2,29 +2,30 @@ import React from 'react';
 import Price from '../utilities/Price';
 import Capitalize from '../utilities/Capitalize';
 
-const Offer = ({ data }) => (
-  <article className="Offer">
-    <div className="flex">
-      <div className="picture">
+const colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple'];
+let count = 0;
+
+const Offer = ({ data }) => {
+  const color = colors[count++ % (colors.length - 1)];
+  return (
+    <article className="Offer">
+      <figure>
         <img src={data.picture} alt="iPhone" />
-      </div>
-      <div className="informations">
-        <h3>
-          <Capitalize string={data.title} />
-        </h3>
+      </figure>
+      <h3>
+        <Capitalize string={data.title} />
+      </h3>
+      <p className="infos">
         <address>{data.location}</address>
-        <div>{data.published.localized}</div>
-        <div>
-          <a href={data.href} target="_blank">
-            Voir
-          </a>
-        </div>
-      </div>
+        <time>{data.published.localized}</time>
+      </p>
       <div className="price">
-        <Price value={data.price} />
+        <a href={data.href} target="_blank" class={`tapped ${color}`}>
+          <Price value={data.price} />
+        </a>
       </div>
-    </div>
-  </article>
-);
+    </article>
+  );
+};
 
 export default Offer;
